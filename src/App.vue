81ad5c00ca9909e18b668e5bbba5ddb4696fc83e
Marijela <marijela.milicevic@student.unipu.hr>
2021-01-02 13:29:13 +0100

    login, singup i promjena ruta

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> 
+      |
+      <router-link to="/login">Prijava</router-link>
+      <router-link to="/singup">Registracija</router-link>
+
     </div>
     <router-view/>
   </div>
