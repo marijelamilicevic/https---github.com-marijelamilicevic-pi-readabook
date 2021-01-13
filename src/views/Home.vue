86f3b59f9e1f987b409082cd62c4189ef0d55582
Marijela <marijela.milicevic@student.unipu.hr>
2021-01-13 15:14:23 +0100

    home uređivanje

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,19 @@
     <div>
       <h1>Read a book</h1>
       <div>
-        <section>
-          <div :style="image" class="image"></div>
-        </section>
-        <div class="tekst">
+        <div id="app">
+          <img src="" />
+        </div>
+        <div class="text">
           <router-link to="/Postojeći korisnik"
             ><button class="btn btn primary">
               <strong>Postojeći korisnik</strong>
             </button></router-link
           >
+          <div class="divider" />
           <router-link to="/Novi korisnik"
             ><button class="btn btn primary">
               <strong>Novi korisnik</strong>
-            </button></router-link
-          >
-          <router-link to="/Glavni pregled"
-            ><button class="btn btn primary">
-              <strong>Baza knjiga</strong>
             </button></router-link
           >
         </div>
@@ -35,9 +31,6 @@ export default {
 </script>
  
 <style scoped>
-image {
-  background-color: saddlebrown;
-}
 .home {
   width: 1518px;
   height: 750px;
@@ -51,15 +44,18 @@ button {
   height: 61px;
   left: 560px;
   top: 421px;
-
   background: #eaca7a;
   border-radius: 10px;
   position: relative;
-
   width: 279px;
   height: 48px;
   left: 580px;
   top: 427px;
+}
+.divider {
+  width: 10px;
+  height: auto;
+  display: inline-block;
 }
 button:hover {
   box-shadow: 9px 9px 7px black;
@@ -70,14 +66,12 @@ h1 {
   height: 174px;
   left: 562px;
   top: 42px;
-
   font-family: Great Vibes;
   font-style: normal;
   font-weight: normal;
   font-size: 70px;
   line-height: 225px;
   text-align: center;
-
   color: #eaca7a;
 }
 </style>
