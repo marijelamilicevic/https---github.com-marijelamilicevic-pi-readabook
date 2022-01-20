1f1f4839c3e3f74d81261c16f5fee22096887fc9
tomislavpercan <tomislav.percan@student.unipu.hr>
2022-01-20 21:25:57 +0100

    komunikacija s firestorom

--- src/views/Rezultati.vue
+++ src/views/Rezultati.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
-    its work
+    {{ $route.params.naslov }}
+{{knjiga}}
+
+    <ul id="example-1">
+  <li v-for="k in knjiga" :key="k.naslov">
+    {{ k.naslov }} - {{k.autor}}
+  </li>
+</ul>
   </div>
 </template>
 
 <script>
+import {firebase} from "@/firebase.js"
+
 export default {
   name: "Rezultati",
+  data: function() {
+    return {
+      knjiga:[]
+      }
+  },
+ 
+  methods: {
+
+    dohvati: function() {
+   
+      firebase.firestore().collection('knjiga').where("zanr", "==", this.$route.params.zanr).where("autor","==", this.$route.params.autor).where("naslov","==", this.$route.params.naslov)
+        .get()
+        .then(querySnapshot => {
+
+          const documents = querySnapshot.docs.map(doc => doc.data())
+          console.log(documents)
+          this.knjiga = documents
+        
+        })
+    }
+
+  },
+  mounted(){
+    this.dohvati()
+
+  }
   
 };
 </script>
